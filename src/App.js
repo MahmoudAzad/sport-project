63c14d5a096633a362bbf5695328990bcb54bdc7
Miladazad1993 <azadmilad1993@gmail.com>
2022-05-28 17:04:12 +0430

    created project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import AppRouter from "./appRouter";
+import 'antd/dist/antd.variable.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import HomeCarousel from "./Components/homeCarousel";
+import { Fragment } from "react";
+const App = () => {
+  return (
+    <Fragment >
+      <AppRouter />
+      <HomeCarousel />
+    </Fragment>
+
+  );
+}
+
+export default App;
---
+++ src/appRouter.jsx
@@ -0,0 +1,18 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+import Home from "./Pages/home";
+
+const AppRouter = () => {
+    return ( 
+        <BrowserRouter>
+            <Routes>
+                <Route path="/"   element = { <Home />} />
+            </Routes>
+        </BrowserRouter>
+     );
+}
+ 
+export default AppRouter;
