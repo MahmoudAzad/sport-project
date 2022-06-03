9c858a17cf740967caee83ddbb045f0dec498e5c
Miladazad1993 <azadmilad1993@gmail.com>
2022-06-03 11:28:27 +0430

    Mmm initial design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'antd/dist/antd.variable.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min.js';
+import './Public/Styles/style.css';
 import HomeCarousel from "./Components/homeCarousel";
 import { Fragment } from "react";
 const App = () => {
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,35 +1,60 @@
+import { SearchOutlined, ShoppingCartOutlined, UnorderedListOutlined, UserOutlined } from '@ant-design/icons';
 import React from 'react';
 
 const Navbar = () => {
     return ( 
-    
-        <nav className="navbar navbar-expand-lg navbar-light  container">
-     
-      
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-              <a className="nav-link" href="#">گیشا مگ  <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="#">پیگیری سفارش <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item m-auto">
+      <div className="navbar-container container mt-3 mb-3">
+          <div className="navbar-caption">
+               <a  href="#">گیشا مگ </a>
+               <a  href="#">پیگیری سفارش </a>
+          </div>
+          <div className="navbar-logo">
               <img src="Gisha-Logotype-200.png" width="150" height="40" />
-            </li>
-           
-     
-          </ul>
-
-
-          
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+          </div>
+          <div className="navbar-icons">
+                <SearchOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+                <ShoppingCartOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+                <UserOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+                <UnorderedListOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+          </div>
+      </div>
      );
 }
  
 export default Navbar;
+
+
+
+
+
+
+
+
+
+      //   <nav className="navbar navbar-expand-lg navbar-light  container mt-3">
+     
+      
+      //   <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      //     <ul className="navbar-nav mr-auto">
+      //     <li className="nav-item active">
+      //         <a  href="#">گیشا مگ </a>
+      //       </li>
+      //       <li className="nav-item active">
+      //         <a  href="#">پیگیری سفارش </a>
+      //       </li>
+      //       <li className="nav-item ">
+      //         <img src="Gisha-Logotype-200.png" width="150" height="40" />
+      //       </li>
+     
+      //     </ul> 
+
+
+          
+      //     <form className="form-inline my-2 my-lg-0">
+      //     <SearchOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+      //     <ShoppingCartOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+      //     <UserOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+      //     <UnorderedListOutlined style={{fontSize:20}} className="p-1 nav-icon" />
+      //     </form>
+      //   </div>
+      // </nav>
