a857a614fe2215518dae1bacda19cbc9c73ca619
Miladazad1993 <azadmilad1993@gmail.com>
2023-04-26 17:53:21 +0430

    removed files

--- src/Components/Function/Paginate.jsx
+++
@@ -1,6 +0,0 @@
-import _ from "lodash";
-
-export const Paginate = (products, currentPage, perPage) => {
-  const startIndex = (currentPage - 1) * perPage;
-  return _(products).slice(startIndex).take(perPage).value();
-};
